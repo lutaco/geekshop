c9d9ba3bb0de59ea85d812a17552d13212729b9c
lutaco <molotkov.1997@mail.ru>
2019-02-08 14:52:39 +0300

    homework-3

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (
-	products_list
+	products_list, product_detail_view
 )
 
 urlpatterns = [
     path('', products_list, name='products'),
+    path('<int:pk>/', product_detail_view, name='detail')
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,24 @@
+import json
 from django.shortcuts import render
 
+from .models import Product
+
 def products_list(request):
-	return render(request, 'products/index.html')
+	data = Product.objects.all()
+	return render(
+		request,
+		'products/index.html',
+		{'object_list': data}
+	)
+
+def product_detail_view(request, pk):
+	# with open('products/fixtures/data/data.json') as file:
+	# 	data = json.load(file)
+
+	data = Product.objects.get(pk=pk)
+	return render(
+		request,
+		'products/detail.html',
+		{'object': data}
+		# {'object': data[idx]}
+	)	
