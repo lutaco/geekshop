8c1a064dce1bffc613a85ac4accf24f9657e5c07
ivan <molotkov.1997@mail.ru>
2019-07-25 19:17:19 +0300

    – Redefined base user model. – Added category model. – Describes the forms for the created models. – Unified form template.

--- products/models.py
+++ products/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+
+class Category(models.Model):
+
+	name = models.CharField(
+		max_length=255
+	)
+
+	description = models.TextField(
+		blank=True,
+		null=True
+	)
+
+	def __str__(self):
+		return self.name
+
+
 class Product(models.Model):
 	name = models.CharField(
 		max_length=255
@@ -18,7 +34,15 @@ class Product(models.Model):
 
 	image = models.ImageField(
 		blank=True,
+		null=True,
 		upload_to="product"
+	)
+
+	category = models.ForeignKey(
+		Category,
+		on_delete=models.CASCADE,
+		blank=True,
+		null=True
 	)
 
 	def __str__(self):
